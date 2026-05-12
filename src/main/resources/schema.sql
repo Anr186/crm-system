f731ea54e7a8269a7cb4598e43a06fd3e9c44cf3
Терентьев Андрей Викторович <ip311s23@csc.sibsutis.ru>
2026-05-12 14:43:24 +0700

    Updated schena

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS sellers (
 CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
     seller_id INTEGER REFERENCES sellers(id),
-    amount DECIMAL(19, 2) NOT NULL,
+    amount DECIMAL(20, 2) NOT NULL,
     payment_type VARCHAR(50) NOT NULL,
     transaction_date TIMESTAMP NOT NULL
 );
